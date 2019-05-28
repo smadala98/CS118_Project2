4079c20e889805ed94d5871603e3480a9feab90a
root <smadala98@gmail.com>
2019-05-27 20:57:21 -0700

    gjht

--- client.c
+++ client.c
@@ -7,18 +7,26 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <time.h>
+
+struct packet {
+  int seq_num;
+  int ack_num;
+  int flags; // If ACK 1st bit is set, SYN 2nd bit is set, FIN 3rd bit is set
+  char payload[512];
+};
 
 int main(int argc, char* argv[]) {
   int sockfd;
   struct sockaddr_in serv_addr;
 
   if (argc != 4) {
-    fprintf(stderr, "Incorrect amount of arguments.");
+    fprintf(stderr, "ERROR: Incorrect amount of arguments.");
     exit(1);
   }
 
   if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-    fprintf(stderr, "Unable to create socket.");
+    fprintf(stderr, "ERROR: Unable to create socket.");
     exit(1);
   }
 
@@ -28,17 +36,23 @@ int main(int argc, char* argv[]) {
 
   struct hostent* hp = gethostbyname(argv[1]);
   if (!hp) {
-    fprintf(stderr, "Host not reachable.");
+    fprintf(stderr, "ERROR: Host not reachable.");
     exit(1);
   }
   memcpy((void *)&serv_addr.sin_addr, hp->h_addr_list[0], hp->h_length);
 
-  char *hello = "Hello from client";
-  if (sendto(sockfd, (const char *)hello, strlen(hello), 0, (const struct sockaddr *) &serv_addr, 
+  struct packet first_packet;
+  srand(time(0));
+  first_packet.seq_num = rand() % 25600;
+  first_packet.flags = (1 << 1);
+  if (sendto(sockfd, &first_packet, sizeof(first_packet), 0, (const struct sockaddr *) &serv_addr, 
 	     sizeof(serv_addr)) < 0) {
-    fprintf(stderr, "Unable to send.");
+    fprintf(stderr, "ERROR: Unable to send.");
     exit(1);
   }
+  int len, n;
+  n = recvfrom(sockfd, &first_packet, sizeof(first_packet), 0, (struct sockaddr *) &serv_addr, &len);
+  printf("%d", first_packet.ack_num);
   close(sockfd);
 }
 
--- server.c
+++ server.c
@@ -10,17 +10,23 @@
 #include <netdb.h>
 #include <sys/time.h>
 
+struct packet {
+  int seq_num;
+  int ack_num;
+  int flags; // 1st bit is set for ACK, 2nd bit is set for SYN, 3rd bit is set for FIN
+};
+  
 int main(int argc, char* argv[]) {
   int sockfd;
   struct sockaddr_in serv_addr, cli_addr;
   
   if (argc != 2) {
-    fprintf(stderr, "Incorrect number of arguments.");
+    fprintf(stderr, "ERROR: Incorrect number of arguments.");
     exit(1);
   }
   
   if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-    fprintf(stderr, "Unable to create socket file descriptor.");
+    fprintf(stderr, "ERROR: Unable to create socket file descriptor.");
     exit(1);
   }
 
@@ -32,14 +38,27 @@ int main(int argc, char* argv[]) {
   serv_addr.sin_port = htons(atoi(argv[1]));
 
   if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-    fprintf(stderr, "Unable to bind socket.");
+    fprintf(stderr, "ERROR: Unable to bind socket.");
     exit(1);
   }
 
+  srand(time(0));
+  int conn_num = 0;
   int len, n;
-  char buffer[1024];
-  n = recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr *) &cli_addr, &len);
-  buffer[n] = '\0';
-  printf("Client : %s\n", buffer);
-  return 0;
+  struct packet p;
+  n = recvfrom(sockfd, &p, sizeof(p), 0, (struct sockaddr *) &cli_addr, &len);
+  printf("%d", n);
+   
+  /*if (p.flags & 2) {
+      conn_num++;
+      struct packet syn_ack_p;
+      syn_ack_p.ack_num = p.seq_num + 1;
+      syn_ack_p.seq_num = rand() % 25600;
+      syn_ack_p.flags = (1 << 1) + 1;
+      if (sendto(sockfd, &syn_ack_p, sizeof(syn_ack_p), 0, (const struct sockaddr *) &cli_addr,
+		 sizeof(cli_addr)) < 0) {
+	fprintf(stderr, "ERROR: Unable to send.");
+	exit(1);
+      }
+      }*/
 }
